f0b89e3137b2f1ba8de198f074ec0dc17220c326
Nathan Lovato <nathan@gdquest.com>
2019-05-17 18:53:13 +0900

    Add a switch to toggle ledge detect on and off

--- prototypes/1.hook-movement/LedgeDetector.gd
+++ prototypes/1.hook-movement/LedgeDetector.gd
@@ -4,6 +4,8 @@ extends Position2D
 onready var ray_bottom: = $RayBottom
 onready var ray_top: = $RayTop
 
+export var active: = true setget set_active
+
 export var ray_length: = 30.0 setget set_ray_length
 
 var _ray_cast_to_x: float = ray_length setget _set_ray_cast_to_x
@@ -16,6 +18,9 @@ func _ready() -> void:
 
 
 func is_against_ledge(look_direction:int) -> bool:
+	if not active:
+		return false
+
 	self._ray_cast_to_x = ray_length * look_direction
 	ray_bottom.force_raycast_update()
 	ray_top.force_raycast_update()
@@ -36,3 +41,7 @@ func _set_ray_cast_to_x(value:float) -> void:
 	ray_top.cast_to = cast_to
 	ray_bottom.position.x = sign(value) * _offset
 	ray_top.position.x = sign(value) * _offset
+
+
+func set_active(value:bool) -> void:
+	active = value
