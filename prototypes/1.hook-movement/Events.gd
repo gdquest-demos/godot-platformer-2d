5464868404dc43639dc081ee763c4324078c0488
Răzvan C. Rădulescu <razcore.art@gmail.com>
2019-05-19 23:03:01 +0900

    Redesign of 1.hook-movement
    Squashed commit of the following:
    
    commit 24ad88f4fa00eaac08acdfef93a0b35ad2b1fc0f
    Merge: a67f0d8 209e41c
    Author: Răzvan C. Rădulescu <razcore.art@gmail.com>
    Date:   Sun May 19 23:01:37 2019 +0900
    
        Merge branch 'redesign' of git://github.com/razcore-art/godot-metroidvania-2d into razcore-art-redesign
    
    commit 209e41cff8ef8ede1a75ebb5d6e45f5d9716e2e4
    Author: Răzvan C. Rădulescu <razcore.art@gmail.com>
    Date:   Sun May 19 14:07:15 2019 +0300
    
        Fix Game scene & project file
    
    commit bfb6f449fa2f8fb54ce7be7cbc67c19eb0cfe4dc
    Merge: a0da29d ae54ee9
    Author: Răzvan C. Rădulescu <razcore.art@gmail.com>
    Date:   Sun May 19 13:45:24 2019 +0300
    
        Merge branch 'master' of https://github.com/GDquest/godot-metroidvania-2d into redesign
    
    commit a0da29d7996101a45c4b64a0e6184430560729cf
    Author: Răzvan C. Rădulescu <razcore.art@gmail.com>
    Date:   Sun May 19 13:36:29 2019 +0300
    
        Reorganize 1.hook-movement folder structure and code

--- prototypes/1.hook-movement/Events.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-signal player_info_updated(dict)
-signal player_state_changed(state_name)
-
-signal zero_gravity_area_entered()
-signal zero_gravity_area_exited()
---
+++ prototypes/1.hook-movement/src/singletons/Events.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+signal player_info_updated(dict)
+signal player_state_changed(state_name)
+
+signal zero_gravity_area_entered()
+signal zero_gravity_area_exited()
