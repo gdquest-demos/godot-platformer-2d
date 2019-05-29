4e1e4c54c1a26e588dc08e10e9608a330dac22b7
Nathan Lovato <nathan@gdquest.com>
2019-05-29 09:11:06 +0900

    Move gamepad input method back to ControlUtils
    There was a bug on the last livestream where Godot wouldn't recognize the method
    on the autoload ControlUtils

--- prototypes/1.hook-movement/src/Player/Hook/Hook.gd
+++ prototypes/1.hook-movement/src/Player/Hook/Hook.gd
@@ -78,21 +78,6 @@ func _get_hook_position() -> Vector2:
 func _get_aim_direction() -> Vector2:
 	match Settings.controls:
 		Settings.GAMEPAD:
-			return get_aim_joystick_direction()
+			return ControlUtils.get_aim_joystick_direction()
 		Settings.KBD_MOUSE, _:
 			return (get_global_mouse_position() - global_position).normalized()
-
-
-# FIXME
-static func get_aim_joystick_direction() -> Vector2:
-	var use_right_stick: bool = ProjectSettings.get_setting('debug/testing/controls/use_right_stick')
-	
-	# FIXME: axes should be 2 and 3, or RX and RY, is there a calibration issue with the gamepad?
-	if use_right_stick:
-		return Vector2(
-			Input.get_joy_axis(0, JOY_AXIS_3), 
-			Input.get_joy_axis(0, JOY_AXIS_4)).normalized()
-	else:
-		return Vector2(
-			Input.get_joy_axis(0, JOY_AXIS_0), 
-			Input.get_joy_axis(0, JOY_AXIS_1)).normalized()
