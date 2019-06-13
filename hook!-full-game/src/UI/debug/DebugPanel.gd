ad3bd1a3c7f86724d8e6bae1c331891c7f7d6338
Henrique Campos <henriquecampos@pigdev.studio>
2019-06-13 10:22:11 -0300

    Add ReferenceName to represent the current node being referenced
    Also remove the reference node name string from the property label's text

--- hook!-full-game/src/UI/debug/DebugPanel.gd
+++ hook!-full-game/src/UI/debug/DebugPanel.gd
@@ -1,7 +1,8 @@
 extends Control
 
-onready var container: VBoxContainer = $MarginContainer/VBoxContainer
+onready var container: VBoxContainer = $VBoxContainer/MarginContainer/VBoxContainer
 onready var reference_node: Node = get_node(reference_node_path)
+onready var name_label: Label = $VBoxContainer/ReferenceName
 
 export (NodePath) var reference_node_path: NodePath
 export (PoolStringArray) var properties: PoolStringArray = []
@@ -18,6 +19,7 @@ func _process(delta) -> void:
 
 func setup() -> void:
 	_clear()
+	name_label.text = reference_node.name
 	for property_string in properties:
 		add_property_label(property_string)
 
@@ -42,5 +44,4 @@ func _update(property: String) -> void:
 		return
 	var search_array: Array = properties
 	var property_label: Label = container.get_child(search_array.find(property))
-	property_label.text = "%s %s: %s"%[reference_node.name, property.capitalize(),
-			reference_node.get(property)]
+	property_label.text = "%s: %s"%[property.capitalize(), reference_node.get(property)]
