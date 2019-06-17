ac948286a67355b6e9065e9741a4ec00d6d0410f
Nathan Lovato <nathan@gdquest.com>
2019-06-17 16:41:25 +0900

    Convert the ledge animation to a capture animation track

--- hook!-full-game/src/Player/Skin.gd
+++ hook!-full-game/src/Player/Skin.gd
@@ -24,6 +24,4 @@ func play(name: String, data: Dictionary = {}) -> void:
 	if name == "ledge":
 		assert 'from' in data
 		position = data.from
-		var anim_ledge: = anim.get_animation(name)
-		anim_ledge.track_set_key_value(0, 0, position)
 	anim.play(name)
