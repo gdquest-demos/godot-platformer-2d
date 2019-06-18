f7d93af7bbbe49bd2529852bd0aeac8a17952e66
Nathan Lovato <nathan@gdquest.com>
2019-06-18 17:09:24 +0900

    Close #60, fix #61: refactor PatrolAgent to use two Position2D nodes for waypoints

--- hook!-full-game/src/AI/PatrolAgent.gd
+++ hook!-full-game/src/AI/PatrolAgent.gd
@@ -1,48 +1,56 @@
 tool
 extends KinematicBody2D
+"""
+Simple AI agent to teach game AI programming basics.
+Patrols between two points, stopping at each point to wait for a moment.
+Detects gaps and walls, and turns around instead of getting stuck or falling in holes.
+"""
 
-const ARRIVE_THRESHOLD: = 5.0
+
+const ARRIVE_THRESHOLD: = 3.0
 
 onready var floor_detector: RayCast2D = $Pivot/FloorDetector
 onready var pivot: Position2D = $Pivot
 onready var timer: Timer = $Timer
 
-export var target_distance: = 400.0 setget set_target_distance
 export var speed: = 300.0
 export var gravity: = 1000.0
 
-onready var waypoints: = {
-	start=global_position,
-	end=global_position + Vector2(target_distance, 0.0)
-}
-onready var _target_position: Vector2 = waypoints.end
+var waypoints: = {}
+var _target: Vector2
 
-var _velocity: = Vector2(speed, gravity)
+var _velocity: = Vector2(0, gravity)
 
 
 func _ready() -> void:
-	timer.connect('timeout', self, '_on_Timer_timeout')
 	if Engine.editor_hint:
-		update()
 		set_physics_process(false)
+	else:
+		timer.connect('timeout', self, '_on_Timer_timeout')
+		position = floor_detector.get_floor_position()
+
+	waypoints = {
+		start=$Start.global_position,
+		end=$End.global_position,
+	}
+	_target = waypoints.end
+	update()
 
 
 func _physics_process(delta: float) -> void:
-	var steering: = Steering.arrive_to(
+	_velocity = Steering.arrive_to(
 						_velocity, 
 						global_position,
-						_target_position,
+						_target,
 						speed)
-	_velocity = move_and_slide(_velocity)
+	move_and_slide(_velocity)
 
-	if not floor_detector.is_close_to_floor():
-		wait()
-	elif global_position.distance_to(_target_position) < ARRIVE_THRESHOLD:
-		global_position.x = _target_position.x
-		wait()
+	if not floor_detector.is_close_to_floor() or \
+		global_position.distance_to(_target) < ARRIVE_THRESHOLD:
+		stop()
 
 
-func wait() -> void:
+func stop() -> void:
 	set_physics_process(false)
 	timer.start()
 
@@ -54,7 +62,7 @@ func walk():
 func turn() -> void:
 	_velocity.x *= -1
 	pivot.scale.x *= -1
-	_target_position = waypoints.start if _target_position == waypoints.end else waypoints.end
+	_target = waypoints.start if _target == waypoints.end else waypoints.end
 
 
 func _on_Timer_timeout() -> void:
@@ -63,29 +71,26 @@ func _on_Timer_timeout() -> void:
 	turn()
 	walk()
 
+
 """
 Draws the path the agent walks in the editor
 """
 func _draw() -> void:
-	if not Engine.editor_hint:
+	if not Engine.editor_hint or not $Start:
 		return
 
 	var draw_radius: = 20.0
 	var line_thickness: = 6.0
-	var target: = Vector2(_target_position.x, 0)
+
+	var start: Vector2 = waypoints.start
+	var end: Vector2 = waypoints.end
 
 	# Path
-	draw_line(Vector2.ZERO, target, DrawingUtils.COLOR_BLUE_LIGHT, line_thickness)
-	draw_circle(Vector2.ZERO, draw_radius, DrawingUtils.COLOR_BLUE_DEEP)
-	draw_circle(target, draw_radius, DrawingUtils.COLOR_BLUE_DEEP)
+	draw_line(start, end, DrawingUtils.COLOR_BLUE_LIGHT, line_thickness)
+	draw_circle(start, draw_radius, DrawingUtils.COLOR_BLUE_DEEP)
+	draw_circle(end, draw_radius, DrawingUtils.COLOR_BLUE_DEEP)
 
 	# Arrow
-	var center: = target / 2
-	var angle: = acos(sign(_target_position.x)) + PI
+	var center: = (start + end) / 2
+	var angle: = start.angle_to(end)
 	DrawingUtils.draw_triangle(self, center, angle, draw_radius)
-
-
-func set_target_distance(value:float) -> void:
-	target_distance = value
-	if Engine.editor_hint:
-		update()
