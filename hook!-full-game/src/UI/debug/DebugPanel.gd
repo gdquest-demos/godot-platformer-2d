8a6252c5ad4fcd94a5f23c6da44c06bc65d8c562
Nathan Lovato <nathan@gdquest.com>
2019-06-18 12:23:15 +0900

    Refactor and tweak DebugPanel to simplify the code and so it better resizes

--- hook!-full-game/src/UI/debug/DebugPanel.gd
+++ hook!-full-game/src/UI/debug/DebugPanel.gd
@@ -14,11 +14,17 @@ export var reference_path: NodePath
 export var properties: PoolStringArray setget set_properties
 
 
+func _ready() -> void:
+	if not reference:
+		return
+	_setup()
+
+
 func _process(delta) -> void:
 	_update()
 
 
-func setup() -> void:
+func _setup() -> void:
 	_clear()
 	_title.text = reference.name
 	for property in properties:
@@ -42,7 +48,7 @@ func _clear() -> void:
 func _update() -> void:
 	var search_array: Array = properties
 	for property in properties:
-		var property_label: Label = _container.get_child(search_array.find(property))
+		var label: Label = _container.get_child(search_array.find(property))
 		var value = reference.get(property)
 
 		var text: = ""
@@ -50,15 +56,17 @@ func _update() -> void:
 			text = "(%01d %01d)" % [value.x, value.y]
 		else:
 			text = str(value)
-		property_label.text = "%s: %s" % [property.capitalize(), text]
+		label.text = "%s: %s" % [property.capitalize(), text]
 
 
 func set_properties(value: PoolStringArray) -> void:
 	properties = value
-	setup()
+	if not reference:
+		return
+	_setup()
 
 
 func set_reference(value: Node) -> void:
 	reference = value
 	if reference:
-	  setup()
+	  _setup()
