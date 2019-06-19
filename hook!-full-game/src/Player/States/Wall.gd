51052823c3a09873d73384af3ab463501ebcae47
Henrique Campos <henriquecampos@pigdev.studio>
2019-06-19 10:35:18 -0300

    Update jump strangth to be an exported variable so we can tweak it faster

--- hook!-full-game/src/Player/States/Wall.gd
+++ hook!-full-game/src/Player/States/Wall.gd
@@ -4,8 +4,9 @@ onready var jump_delay: Timer = $JumpDelay
 
 export var wall_slide_acceleration: = 100.0
 export var max_wall_slide_speed: = 200.0
+export (float, 0.0, 1.0) var wall_friction_factor: = 0.15
 
-var _jump_strength: = 1000.0
+export var jump_strength: = 1000.0
 var _velocity: = Vector2.ZERO
 var _wall_normal: = -1
 
@@ -14,18 +15,20 @@ func setup(player: KinematicBody2D, state_machine: Node) -> void:
 	.setup(player, state_machine)
 	jump_delay.connect("timeout", self, "_on_JumpDelay_timeout")
 
-
+	
 func enter(msg: Dictionary = {}) -> void:
 	_wall_normal = msg.normal
 	_velocity = msg.velocity
 	if _velocity.y < max_wall_slide_speed:
 		_velocity.y = 0.0
+	if not Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+		_state_machine.transition_to("Move/Idle")
+		return
 
 
 func physics_process(delta: float) -> void:
 	if _velocity.y > max_wall_slide_speed:
-		_velocity.y = lerp(_velocity.y, max_wall_slide_speed, 0.15)
-		print("friction")
+		_velocity.y = lerp(_velocity.y, max_wall_slide_speed, wall_friction_factor)
 	else:
 		_velocity.y += wall_slide_acceleration * delta
 	clamp(_velocity.y, 0.0, max_wall_slide_speed)
@@ -37,7 +40,7 @@ func physics_process(delta: float) -> void:
 
 func unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
-		var jump_velocity: = Vector2(_wall_normal, -1.0).normalized() * _jump_strength
+		var jump_velocity: = Vector2(_wall_normal, -1.0).normalized() * jump_strength
 		
 		_state_machine.transition_to("Move/Air", {"velocity": jump_velocity})
 	
