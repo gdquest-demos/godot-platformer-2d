241ac9203a7a1d7052dce55cb359edabd096b4b2
Nathan Lovato <nathan@gdquest.com>
2019-06-20 16:07:53 +0900

    Close #75: Increase wall slide speed, refactor wall state
    Mostly renamed some variables, removed 2 unused lines

--- hook!-full-game/src/Player/States/Wall.gd
+++ hook!-full-game/src/Player/States/Wall.gd
@@ -2,9 +2,9 @@ extends State
 
 onready var jump_delay: Timer = $JumpDelay
 
-export var wall_slide_acceleration: = 100.0
-export var max_wall_slide_speed: = 200.0
-export (float, 0.0, 1.0) var wall_friction_factor: = 0.15
+export var slide_acceleration: = 500.0
+export var max_slide_speed: = 400.0
+export (float, 0.0, 1.0) var friction_factor: = 0.15
 
 export var jump_strength: = 1000.0
 var _velocity: = Vector2.ZERO
@@ -19,18 +19,15 @@ func setup(player: KinematicBody2D, state_machine: Node) -> void:
 func enter(msg: Dictionary = {}) -> void:
 	_wall_normal = msg.normal
 	_velocity = msg.velocity
-	if _velocity.y < max_wall_slide_speed:
+	if _velocity.y < max_slide_speed:
 		_velocity.y = 0.0
-	if not Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
-		_state_machine.transition_to("Move/Idle")
-		return
 
 
 func physics_process(delta: float) -> void:
-	if _velocity.y > max_wall_slide_speed:
-		_velocity.y = lerp(_velocity.y, max_wall_slide_speed, wall_friction_factor)
+	if _velocity.y > max_slide_speed:
+		_velocity.y = lerp(_velocity.y, max_slide_speed, friction_factor)
 	else:
-		_velocity.y += wall_slide_acceleration * delta
+		_velocity.y += slide_acceleration * delta
 	_velocity = owner.move_and_slide(_velocity, owner.FLOOR_NORMAL)
 
 	if owner.is_on_floor():
@@ -43,8 +40,8 @@ func unhandled_input(event: InputEvent) -> void:
 		
 		_state_machine.transition_to("Move/Air", {"velocity": jump_velocity})
 	
-	if ((event.is_action_released("move_left") and _wall_normal > 0) or
-			(event.is_action_released("move_right") and _wall_normal < 0)):
+	if (event.is_action_released("move_left") and _wall_normal > 0) or \
+	   (event.is_action_released("move_right") and _wall_normal < 0):
 		jump_delay.start()
 
 
