aec8226a8b52758a50d2c652cff98acde49ce060
Nathan Lovato <nathan@gdquest.com>
2019-07-16 17:32:12 +0200

    Start adding game architecture, Game scene and LevelLoader node

---
+++ hook!-full-game/src/Main/LevelLoader.gd
@@ -0,0 +1,42 @@
+tool
+extends Node
+"""
+Loads and unloads levels
+"""
+
+signal loaded(level)
+
+export var LEVEL_START: PackedScene
+export var player_path: NodePath
+
+var level: Node
+onready var player: Player = get_node(player_path)
+
+
+func _ready():
+	if LEVEL_START:
+		setup(LEVEL_START)
+
+
+func setup(new_level: PackedScene) -> void:
+	remove_child(player)
+	change_level(new_level)
+
+
+func change_level(level_new: PackedScene) -> void:
+	if level:
+		level.queue_free()
+
+	level = level_new.instance()
+	add_child(level)
+	level.add_child(player)
+	emit_signal("loaded", level)
+
+
+func _get_configuration_warning() -> String:
+	var warning: = ""
+	if not LEVEL_START:
+		warning += "%s is missing a start level, consider setting one in the inspector.\n" % name
+	if not player:
+		warning += "%s is missing the player node as its child" % name
+	return warning
