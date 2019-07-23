71f1a6ac5e576f12866e33e77f5eb5df92d51184
Henrique Campos <henriquecampos@pigdev.studio>
2019-07-23 10:21:29 -0300

    Add Level Design Dock plugin v0.1.0

---
+++ hook!-full-game/addons/level_design_dock/LevelDesignDock.gd
@@ -0,0 +1,35 @@
+tool
+extends Control
+
+signal packed_scene_button_created(button)
+onready var grid_container: GridContainer = $VBoxContainer/GridContainer
+
+const PackedSceneButtonScene: = preload("res://addons/level_design_dock/PackedSceneButton.tscn")
+const PackedSceneButton: = preload("res://addons/level_design_dock/PackedSceneButton.gd")
+
+func drop_data(position: Vector2, data: Dictionary) -> void:
+	for packed_scene_path in get_valid_files(data["files"]):
+		create_packed_scene_button(packed_scene_path)
+
+
+func can_drop_data(position: Vector2, data: Dictionary) -> bool:
+	var can_drop: = false
+	if data["type"] == "files":
+		can_drop = get_valid_files(data["files"]).size() > 0
+	return can_drop
+
+
+func create_packed_scene_button(packed_scene_path: String) -> void:
+	var new_button: = PackedSceneButtonScene.instance() as PackedSceneButton
+	new_button.packed_scene = load(packed_scene_path)
+	new_button.text = packed_scene_path.get_file().trim_suffix(".tscn")
+	grid_container.add_child(new_button)
+	emit_signal("packed_scene_button_created", new_button)
+
+
+func get_valid_files(files_list: Array) -> Array:
+	var valid_files: Array = []
+	for file_path in files_list:
+		if file_path.ends_with(".tscn"):
+			valid_files.append(file_path)
+	return valid_files
---
+++ hook!-full-game/addons/level_design_dock/plugin.gd
@@ -0,0 +1,96 @@
+tool
+extends EditorPlugin
+
+const LevelDesignDockScene: = preload("res://addons/level_design_dock/LevelDesignDock.tscn")
+const LevelDesignDock: = preload("res://addons/level_design_dock/LevelDesignDock.gd")
+const PackedSceneButton: = preload("res://addons/level_design_dock/PackedSceneButton.gd")
+
+var _interface: LevelDesignDock
+var _current_instance: Node2D setget _set_current_instance
+var _current_parent: Node setget _set_current_parent
+var _current_scene: PackedScene setget _set_current_scene
+
+func _enter_tree() -> void:
+	_interface = LevelDesignDockScene.instance()
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_UR, _interface)
+	_interface.connect("packed_scene_button_created", self, "_on_Interface_PackedScene_Button_Created")
+
+
+func _exit_tree() -> void:
+	_interface.queue_free()
+
+
+func handles(object: Object) -> bool:
+	var can_handle = object is Node
+	if can_handle:
+		self._current_parent = object
+	return can_handle
+
+
+func forward_canvas_gui_input(event: InputEvent) -> bool:
+	var forward: = false
+	if event is InputEventMouseMotion:
+		_move_instance()
+		forward = true
+	elif event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			forward = true
+			if event.is_pressed():
+				_create_instance()
+			else:
+				_current_instance = null
+	if event is InputEventKey:
+		if event.scancode == KEY_ESCAPE:
+			_current_instance = null
+			_current_parent = null
+			_current_scene = null
+			forward = true
+	return forward
+
+
+func _move_instance() -> void:
+	var can_drag = Input.is_mouse_button_pressed(BUTTON_LEFT) and _current_instance
+	if can_drag:
+		_current_instance.global_position = _current_instance.get_global_mouse_position()
+
+
+func _on_Interface_PackedScene_Button_Created(button: PackedSceneButton) -> void:
+	button.connect("toggled", self, "_on_PackedSceneButton_button_toggled", [button])
+
+
+func _on_PackedSceneButton_button_toggled(button_pressed: bool, button: PackedSceneButton):
+	if button_pressed:
+		self._current_scene = button.packed_scene
+	else:
+		_current_scene = null
+
+
+func _can_instance() -> bool:
+	return _current_parent and _current_scene
+
+
+func _create_instance() -> void:
+	if not _can_instance():
+		return
+	get_editor_interface().get_selection().clear()
+	var instance = _current_scene.instance()
+	var undo = get_undo_redo()
+	self._current_instance = instance
+	
+	undo.create_action("Add child")
+	undo.add_do_method(_current_parent, "add_child", instance)
+	undo.add_undo_method(_current_parent, "remove_child", instance)
+	undo.commit_action()
+	instance.owner = get_editor_interface().get_edited_scene_root()
+
+
+func _set_current_instance(new_instance: Node2D) -> void:
+	_current_instance = new_instance
+
+
+func _set_current_scene(new_packed_scene: PackedScene) -> void:
+	_current_scene = new_packed_scene
+
+
+func _set_current_parent(new_parent: Node) -> void:
+	_current_parent = new_parent
