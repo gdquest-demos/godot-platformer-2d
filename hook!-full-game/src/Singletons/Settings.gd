2ec600880359b8173c10644d0249b8fb76d57e83
Nathan Lovato <nathan@gdquest.com>
2019-07-25 18:09:42 +0200

    Fix #112: Prevent input scheme switch if the mouse is outside the game window

--- hook!-full-game/src/Singletons/Settings.gd
+++ hook!-full-game/src/Singletons/Settings.gd
@@ -8,7 +8,7 @@ var controls: = KBD_MOUSE setget set_controls
 func _input(event: InputEvent) -> void:
 	if event is InputEventJoypadButton or event is InputEventJoypadMotion:
 		self.controls = GAMEPAD
-	else:
+	elif event is InputEventMouse:
 		self.controls = KBD_MOUSE
 
 func set_controls(value:int) -> void:
