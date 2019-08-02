9042f2540da3e8090e9f4bd3164d8ac10ad0d999
Nathan Lovato <nathan@gdquest.com>
2019-08-02 16:08:33 +0200

    Close #135: Remove Godot built-in deadzone for the joystick, use circular deadzone

--- game/src/Autoload/Utils.gd
+++ game/src/Autoload/Utils.gd
@@ -12,11 +12,16 @@ static func get_aim_joystick_direction() -> Vector2:
 """
 Returns the strength of the XY input with the joystick used for aiming,
 each axis being a value between 0 and 1
+Adds a circular deadzone, as Godot's built-in system is per-axis,
+creating a rectangular deadzone on the joystick
 """
 static func get_aim_joystick_strength() -> Vector2:
-	return Vector2(
+	var deadzone_radius: = 0.5
+	var input_strength: = Vector2(
 		Input.get_action_strength("aim_right") - Input.get_action_strength("aim_left"),
-		Input.get_action_strength("aim_down") - Input.get_action_strength("aim_up"))
+		Input.get_action_strength("aim_down") - Input.get_action_strength("aim_up")
+	)
+	return input_strength if input_strength.length() > deadzone_radius else Vector2.ZERO
 
 """
 Checks if two numbers are approximately equal
