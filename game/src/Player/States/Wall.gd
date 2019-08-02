a9732e6568c6eef8863efd6c8b28037f8c996503
Nathan Lovato <nathan@gdquest.com>
2019-08-02 14:42:47 +0200

    Close #133: Improve wall movement feel, simplify code
    Retain inertia after a jump up towards the wall
    Remove _velocity property: use move.velocity instead
    Increase slide acceleration
    Make jump impulse a Vector2
    Clamp Y velocity to avoid movement bugs

--- game/src/Player/States/Wall.gd
+++ game/src/Player/States/Wall.gd
@@ -1,75 +1,68 @@
 tool
 extends State
 
-onready var jump_delay: Timer = $JumpDelay
 onready var fall_delay: Timer = $FallDelay
 
-export var slide_acceleration: = 500.0
+export var slide_acceleration: = 1600.0
 export var max_slide_speed: = 400.0
 export (float, 0.0, 1.0) var friction_factor: = 0.15
 
-export var jump_strength: = 1000.0
-var _velocity: = Vector2.ZERO
+export var jump_strength: = Vector2(500.0, 900.0)
 var _wall_normal: = -1
+var _velocity: = Vector2.ZERO
 
 
 func _ready() -> void:
-	jump_delay.connect("timeout", self, "_on_JumpDelay_timeout")
 	fall_delay.connect("timeout", self, "_on_FallDelay_timeout")
 
 
 func _get_configuration_warning() -> String:
-	return "" if $JumpDelay and $FallDelay else "%s requires two Timer children named JumpDelay and FallDelay" % name
+	return "" if $FallDelay else "%s requires a Timer child named FallDelay" % name
 
 
 func enter(msg: Dictionary = {}) -> void:
 	_wall_normal = msg.normal
-	_velocity = msg.velocity
-	if _velocity.y < max_slide_speed:
-		_velocity.y = 0.0
-	
-	var wall_detector_length: float = owner.wall_detector.cast_to.length()
-	var wall_direction: = -_wall_normal
-	owner.wall_detector.cast_to = Vector2(wall_direction * wall_detector_length, 0)
+	var move: State = get_parent()
+	_velocity.y = clamp(msg.velocity.y, -max_slide_speed, max_slide_speed)
+
+	owner.wall_detector.cast_to.x = abs(owner.wall_detector.cast_to.x) * -1.0 * sign(_wall_normal)
 	owner.wall_detector.enabled = true
 
 
 func physics_process(delta: float) -> void:
+	var move: State = get_parent()
 	if _velocity.y > max_slide_speed:
 		_velocity.y = lerp(_velocity.y, max_slide_speed, friction_factor)
 	else:
 		_velocity.y += slide_acceleration * delta
+	_velocity.y = clamp(_velocity.y, -max_slide_speed, max_slide_speed)
 	_velocity = owner.move_and_slide(_velocity, owner.FLOOR_NORMAL)
 
 	if owner.is_on_floor():
 		_state_machine.transition_to("Move/Idle")
-	
+
 	if not owner.wall_detector.is_colliding():
 		if fall_delay.is_stopped():
 			fall_delay.start()
 	else:
 		fall_delay.stop()
 
+	var is_moving_away_from_wall: = sign(move.get_move_direction().x) == sign(_wall_normal)
+	if is_moving_away_from_wall:
+		_state_machine.transition_to("Move/Air")
+
 
 func unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
-		var jump_velocity: = Vector2(_wall_normal, -1.0).normalized() * jump_strength
-		
-		_state_machine.transition_to("Move/Air", {"velocity": jump_velocity})
-	
-	if (event.is_action_released("move_left") and _wall_normal > 0) or \
-	   (event.is_action_released("move_right") and _wall_normal < 0):
-		jump_delay.start()
+		var impulse: = Vector2(_wall_normal, -1.0).normalized() * jump_strength
+		_state_machine.transition_to("Move/Air", {"velocity": impulse})
 
 
 func exit() -> void:
-	_velocity = Vector2.ZERO
 	owner.wall_detector.enabled = false
-
-
-func _on_JumpDelay_timeout() -> void:
-	_state_machine.transition_to("Move/Air", {"velocity": _velocity.y})
+	fall_delay.stop()
 
 
 func _on_FallDelay_timeout() -> void:
-	_state_machine.transition_to("Move/Air", {"velocity": _velocity.y})
+	var move: State = get_parent()
+	_state_machine.transition_to("Move/Air", {"velocity": _velocity})
