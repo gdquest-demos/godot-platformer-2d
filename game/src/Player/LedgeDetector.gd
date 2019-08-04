e5f4fdbc092da2c043a06f27a8053131a43fcefc
Nathan Lovato <nathan@gdquest.com>
2019-08-04 10:28:18 +0200

    Close #113: Simplify and improve LedgeDetector
    LedgeDetector should ignore pass-through platforms, it only detects bodies on
    the World Physics layer now
    Shorten and reposition the rays so that the ledge helper is subtle
    Don't reset X velocity in ledge state
    Transition to Run instead of idle to preserve player inertia

--- game/src/Player/LedgeDetector.gd
+++ game/src/Player/LedgeDetector.gd
@@ -5,41 +5,35 @@ Detects ledges using two Raycasts casting horizontally.
 If one ray is in a wall and the other is in the air, it means the node is near a ledge.
 """
 
+
 onready var ray_bottom: RayCast2D = $RayBottom
 onready var ray_top: RayCast2D = $RayTop
 
-onready var _offset: float = ray_bottom.position.x
-
 export var is_active: = true
 
-export var ray_length: = 30.0 setget set_ray_length
 
-var _ray_cast_to_x: = ray_length setget _set_ray_cast_to_x
-
-
-func _ready() -> void:
-	self.ray_length = ray_length
+func _ready():
+	assert ray_top.cast_to.x >= 0
+	assert ray_bottom.cast_to.x >= 0
 
 
 func is_against_ledge(look_direction: int) -> bool:
 	if not is_active:
 		return false
 
-	self._ray_cast_to_x = ray_length * look_direction
+	scale.x = look_direction
 	ray_bottom.force_raycast_update()
 	ray_top.force_raycast_update()
 	return ray_bottom.is_colliding() and not ray_top.is_colliding()
 
 
-func set_ray_length(value: float) -> void:
-	ray_length = value
-	self._ray_cast_to_x = value * sign(_ray_cast_to_x)
+func get_cast_to_directed() -> Vector2:
+	return Vector2(ray_top.cast_to.x * scale.x, 0.0)
 
 
-func _set_ray_cast_to_x(value: float) -> void:
-	_ray_cast_to_x = value
-	var cast_to: = Vector2(value, 0.0)
-	ray_bottom.cast_to = cast_to
-	ray_top.cast_to = cast_to
-	ray_bottom.position.x = sign(value) * _offset
-	ray_top.position.x = sign(value) * _offset
+func get_top_global_position() -> Vector2:
+	return ray_top.global_position
+
+
+func get_ray_length() -> float:
+	return ray_top.cast_to.x
--- game/src/Player/States/Ledge.gd
+++ game/src/Player/States/Ledge.gd
@@ -10,18 +10,15 @@ func _setup() -> void:
 
 func _on_Skin_animation_finished(name: String) -> void:
 	if name == "ledge":
-		_state_machine.transition_to("Move/Idle")
+		_state_machine.transition_to("Move/Run")
 
 
 func enter(msg: Dictionary = {}) -> void:
-	if not "move_state" in msg:
-		return
+	assert "move_state" in msg and msg.move_state is State
 
 	var start: Vector2 = owner.global_position
-	owner.global_position = (
-			owner.ledge_detector.ray_top.global_position
-			+ Vector2(owner.ledge_detector.ray_length * sign(msg.move_state.velocity.x), 0.0))
+	var ld = owner.ledge_detector
+	owner.global_position = ld.get_top_global_position() + ld.get_cast_to_directed()
 	owner.global_position = owner.floor_detector.get_floor_position()
-
-	msg.move_state.velocity = Vector2.ZERO
+	msg.move_state.velocity.y = 0.0
 	owner.skin.play('ledge', {from = start - owner.global_position})
