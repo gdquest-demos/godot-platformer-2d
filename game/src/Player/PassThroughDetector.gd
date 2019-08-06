c59516b1e539ce7517222154180542bcee81c95c
Nathan Lovato <nathan@gdquest.com>
2019-08-06 16:54:04 +0200

    Split PassThroughDetector from the player, turn physics_process off in editor

--- game/src/Player/PassThroughDetector.gd
+++ game/src/Player/PassThroughDetector.gd
@@ -9,11 +9,8 @@ export var actor_path: = NodePath("..")
 
 func _ready() -> void:
 	connect("body_exited", self, "_on_body_exited")
-
-
-func _physics_process(delta: float) -> void:
-	if not Engine.editor_hint and _actor.is_on_floor() and not _actor.get_collision_mask_bit(PASS_THROUGH_LAYER):
-		_actor.set_collision_mask_bit(PASS_THROUGH_LAYER, true)
+	if Engine.editor_hint:
+		set_physics_process(false)
 
 
 func _on_body_exited(body: CollisionObject2D) -> void:
@@ -21,10 +18,15 @@ func _on_body_exited(body: CollisionObject2D) -> void:
 		_actor.set_collision_mask_bit(PASS_THROUGH_LAYER, true)
 
 
+func _physics_process(delta: float) -> void:
+	if _actor.is_on_floor() and not _actor.get_collision_mask_bit(PASS_THROUGH_LAYER):
+		_actor.set_collision_mask_bit(PASS_THROUGH_LAYER, true)
+
+
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("jump"):
-		if Input.is_action_pressed("move_down"):
-			_actor.set_collision_mask_bit(PASS_THROUGH_LAYER, false)
+	if Input.is_action_pressed("move_down") and _actor.is_on_floor():
+		_actor.set_collision_mask_bit(PASS_THROUGH_LAYER, false)
+
 
 func _get_configuration_warning() -> String:
 	return "Actor Path must be defined" if actor_path.is_empty() else ""
