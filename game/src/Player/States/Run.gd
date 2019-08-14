c31e970772b6abc8a7564845ecb3c3cabe990e98
Răzvan C. Rădulescu <razcore.art@gmail.com>
2019-08-14 10:33:46 +0300

    Replace get_parent() calls with _parent var

--- game/src/Player/States/Run.gd
+++ game/src/Player/States/Run.gd
@@ -8,23 +8,21 @@ with state transitions
 
 
 func unhandled_input(event: InputEvent) -> void:
-	var move: = get_parent()
-	move.unhandled_input(event)
+	_parent.unhandled_input(event)
 
 
 func physics_process(delta: float) -> void:
-	var move: = get_parent()
 	if owner.is_on_floor():
-		if move.get_move_direction().x == 0.0:
+		if _parent.get_move_direction().x == 0.0:
 			_state_machine.transition_to("Move/Idle")
 	else:
 		_state_machine.transition_to("Move/Air")
-	move.physics_process(delta)
+	_parent.physics_process(delta)
 
 
 func enter(msg: Dictionary = {}) -> void:
-	get_parent().enter(msg)
+	_parent.enter(msg)
 
 
 func exit() -> void:
-	get_parent().exit()
+	_parent.exit()
