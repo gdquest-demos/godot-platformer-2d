8ae2a95e30695a776dc13ff075541e389a61ca00
Nathan Lovato <nathan@gdquest.com>
2019-08-21 17:22:36 +0200

    Fix bug with floor not detected in ledge state

--- game/src/Player/FloorDetector.gd
+++ game/src/Player/FloorDetector.gd
@@ -10,9 +10,11 @@ func is_close_to_floor() -> bool:
 
 
 func get_floor_position() -> Vector2:
+	force_raycast_update()
 	return get_collision_point()
 
 
 func get_floor_distance_ratio() -> float:
+	force_raycast_update()
 	var ratio: = 1.0 - abs(get_collision_point().y - global_position.y) / cast_to.y
 	return ratio
