42bb4cbecda917b2aed92ea44e749305ec4e1ef3
Nathan Lovato <nathan@gdquest.com>
2019-09-07 10:04:25 +0200

    Close #159: Remove aim logic in the Fire state
    Now the Fire state doesn't depend on the Aim state anymore so I removed
    the parenting

--- game/src/Player/Hook/States/Fire.gd
+++ game/src/Player/Hook/States/Fire.gd
@@ -5,10 +5,6 @@ func _on_Cooldown_timeout() -> void:
 	_state_machine.transition_to("Aim")
 
 
-func physics_process(delta: float) -> void:
-	_parent.physics_process(delta)
-
-
 func enter(msg: Dictionary = {}) -> void:
 	owner.cooldown.connect("timeout", self, "_on_Cooldown_timeout")
 
@@ -17,7 +13,7 @@ func enter(msg: Dictionary = {}) -> void:
 	var target: HookTarget = owner.snap_detector.target
 	if not target:
 		var distance: = min(owner._radius, owner.get_local_mouse_position().length())
-		owner.arrow.hook_position = owner.global_position + owner.get_local_mouse_position().normalized() * distance
+		owner.arrow.hook_position = owner.global_position + owner.get_local_mouse_position().normalized() * distance / 2.0
 		_state_machine.transition_to("Aim")
 		return
 
