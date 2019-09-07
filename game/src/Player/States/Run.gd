fb5b1f5996e4036268921fb34675aaa13e29967c
Nathan Lovato <nathan@gdquest.com>
2019-09-07 10:32:14 +0200

    Close #134: Add time limit and gradual slow down after a power jump

--- game/src/Player/States/Run.gd
+++ game/src/Player/States/Run.gd
@@ -5,6 +5,28 @@ Delegates movement to its parent Move state and extends it
 with state transitions
 """
 
+onready var slow_starter: Timer = $SlowStarter
+onready var tween: Tween = $Tween
+
+export var slow_duration_seconds: = 0.4
+
+
+func _ready() -> void:
+	slow_starter.connect("timeout", self, "_on_SlowDown_timeout")
+
+
+func _on_SlowDown_timeout() -> void:
+	tween.interpolate_property(
+		_parent,
+		"max_speed",
+		_parent.max_speed,
+		_parent.max_speed_default,
+		slow_duration_seconds,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_OUT
+	)
+	tween.start()
+
 
 func unhandled_input(event: InputEvent) -> void:
 	_parent.unhandled_input(event)
@@ -21,6 +43,8 @@ func physics_process(delta: float) -> void:
 
 func enter(msg: Dictionary = {}) -> void:
 	_parent.enter(msg)
+	if not Utils.is_equal_approx(_parent.max_speed.x, _parent.max_speed_default.x):
+		slow_starter.start()
 
 
 func exit() -> void:
