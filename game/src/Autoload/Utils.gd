dac4ff1bd76befede0d1bd0fe8ad95067b39884d
Nathan Lovato <nathan@gdquest.com>
2020-01-11 15:58:11 +0100

    Fix comments after converting docstrings
    Some files were not converted properly, leaving the game in a broken state.

--- game/src/Autoload/Utils.gd
+++ game/src/Autoload/Utils.gd
@@ -1,27 +1,26 @@
+extends Node
 # Globally accessible utils functionality
-# extends Node
 
-# 
 # Returns the direction in which the player is aiming with the stick
-# static func get_aim_joystick_direction() -> Vector2:
-	# return get_aim_joystick_strength().normalized()
+static func get_aim_joystick_direction() -> Vector2:
+	return get_aim_joystick_strength().normalized()
 
-# 
+
 # Returns the strength of the XY input with the joystick used for aiming,
-each axis being a value between 0 and 1
-Adds a circular deadzone, as Godot's built-in system is per-axis,
-creating a rectangular deadzone on the joystick
-# static func get_aim_joystick_strength() -> Vector2:
-	# var deadzone_radius: = 0.5
-	# var input_strength: = Vector2(
-		# Input.get_action_strength("aim_right") - Input.get_action_strength("aim_left"),
-		# Input.get_action_strength("aim_down") - Input.get_action_strength("aim_up")
-	# )
-	# return input_strength if input_strength.length() > deadzone_radius else Vector2.ZERO
+# each axis being a value between 0 and 1
+# Adds a circular deadzone, as Godot's built-in system is per-axis,
+# creating a rectangular deadzone on the joystick
+static func get_aim_joystick_strength() -> Vector2:
+	var deadzone_radius: = 0.5
+	var input_strength: = Vector2(
+		Input.get_action_strength("aim_right") - Input.get_action_strength("aim_left"),
+		Input.get_action_strength("aim_down") - Input.get_action_strength("aim_up")
+	)
+	return input_strength if input_strength.length() > deadzone_radius else Vector2.ZERO
 
 # Checks if two numbers are approximately equal
-# static func is_equal_approx(a: float, b: float, cmp_epsilon: float = 1e-5) -> bool:
-	# var tolerance: = cmp_epsilon * abs(a)
-	# if tolerance < cmp_epsilon:
-		# tolerance = cmp_epsilon
-	# return abs(a - b) < tolerance
+static func is_equal_approx(a: float, b: float, cmp_epsilon: float = 1e-5) -> bool:
+	var tolerance: = cmp_epsilon * abs(a)
+	if tolerance < cmp_epsilon:
+		tolerance = cmp_epsilon
+	return abs(a - b) < tolerance
--- game/src/Player/Camera/CameraRig.gd
+++ game/src/Player/Camera/CameraRig.gd
@@ -16,16 +16,16 @@ func _physics_process(delta: float) -> void:
 
 func update_position(velocity: Vector2 = Vector2.ZERO) -> void:
 	# Updates the camera rig's position based on the player's state and controller position
-	# if not is_active:
-		# return
+	if not is_active:
+		return
 
-# 	# match Settings.controls:
+	match Settings.controls:
 
-# 		# Settings.GAMEPAD:
-			# var joystick_strength: = Utils.get_aim_joystick_strength()
-			# camera.position = joystick_strength * offset
+		Settings.GAMEPAD:
+			var joystick_strength: = Utils.get_aim_joystick_strength()
+			camera.position = joystick_strength * offset
 
-# 		# Settings.KBD_MOUSE, _:
-			# var mouse_position: = get_local_mouse_position()
-			# var distance_ratio: = clamp(mouse_position.length(), mouse_range.x, mouse_range.y) / mouse_range.y
-			# camera.position = distance_ratio * mouse_position.normalized() * offset
+		Settings.KBD_MOUSE, _:
+			var mouse_position: = get_local_mouse_position()
+			var distance_ratio: = clamp(mouse_position.length(), mouse_range.x, mouse_range.y) / mouse_range.y
+			camera.position = distance_ratio * mouse_position.normalized() * offset
